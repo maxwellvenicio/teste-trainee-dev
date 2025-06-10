59831763553f1fd22055c1a8e1d54b7dbf970ed6
Maxwell Venicio <maxwellvenicio@gmail.com>
2025-06-10 00:44:14 -0300

    feat: implemente filtro de palavrões

--- src/app/todo/new-task/new-task.component.ts
+++ src/app/todo/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Todo } from '../../shared/models/todo.model';
 import { TodoService } from '../../shared/services/todo.service';
+import { Filter } from 'bad-words';
 
 @Component({
   selector: 'app-new-task',
@@ -9,18 +10,37 @@ import { TodoService } from '../../shared/services/todo.service';
 })
 export class NewTaskComponent {
   newTaskTitle: string = '';
-  showError: boolean = false;
+  showProfanityError: boolean = false;
 
   constructor(private todoService: TodoService) { }
 
   addTask() {
+    this.showProfanityError = false;
 
-    if(!this.newTaskTitle.trim()) {
-      this.showError = true;
-      return;
-    }
-    
+    const filter = new Filter()
+    filter.addWords(
+      'Porra',
+      'Foda',
+      'Escroto',
+      'Escrota',
+      'Caralho',
+      'Puta',
+      'Puto',
+      'Cacete',
+      'Fudeu',
+      'Merda',
+      'Bosta',
+    );
+
     const titles = this.newTaskTitle.split('|');
+
+    for(const title of titles){
+      const cleanTitle = title.trim();
+      if(cleanTitle && filter.isProfane(cleanTitle)){
+        this.showProfanityError = true;
+        return;
+      }
+    }
 
     titles.forEach(title => {
       const cleanTitle = title.trim();
@@ -33,6 +53,7 @@ export class NewTaskComponent {
        this.todoService.addTodo(newTodo);
       }
     });
+
     this.newTaskTitle = '';
   }
 }
